bec6cd68a7bf2732dd6207e6efbf44bdcef3808a
Diego Rapoport <diegomrapoport@gmail.com>
2022-11-18 09:36:18 -0300

    Import programatically inside config

--- lua/didi/init.lua
+++ lua/didi/init.lua
@@ -1,4 +1,18 @@
-require 'options'
+local subarquivos = require('utils.caminhos').gera_lista_subarquivos
+local nome_arq = require('utils.caminhos').pega_nome_arquivo
+local limpa_str = require('utils.strings').limpa_str
+
+local configs = subarquivos('/home/dinhego/.config/nvim/lua')
+for _, conf in ipairs(configs) do
+  -- Lembrar de adicionar aqui caso pastas sejam adicionadas
+  if(string.find(conf, 'didi') or string.find(conf, 'utils') or string.find(conf, 'ftplugin')) then goto continue end
+  local arquivo = nome_arq(conf)
+  local importar = limpa_str(arquivo, {'.lua'})
+  require(importar)
+  ::continue::
+end
+
+--[[ require 'options'
 require 'keymaps'
 require 'plugins'
 require 'colors'
@@ -11,4 +25,4 @@ require '_telescope'
 require '_toggleterm'
 require '_startup'
 require 'autocmds'
-require '_neorg'
+require '_neorg' ]]
