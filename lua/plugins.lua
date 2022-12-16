eb96d7fb719ac9aa150cde578a737465a26df3e4
Diego Rapoport <diegomrapoport@gmail.com>
2022-12-16 14:20:51 -0300

    Add nvim tree

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,304 +4,312 @@ local notify = vim.notify
 -- Instala packer automagicamente
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
-	print("Instalando packer, feche e reabra o neovim...")
-	vim.cmd([[packadd packer.nvim]])
+  PACKER_BOOTSTRAP = fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  })
+  print("Instalando packer, feche e reabra o neovim...")
+  vim.cmd([[packadd packer.nvim]])
 end
 
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-	notify("Status " .. status_ok .. " Packer")
-	return
+  notify("Status " .. status_ok .. " Packer")
+  return
 end
 
 packer.init({
-	-- packer roda num popup
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-	},
-	auto_clean = false, -- não remove pacotes não utilizados
+  -- packer roda num popup
+  display = {
+    open_fn = function()
+      return require("packer.util").float({ border = "rounded" })
+    end,
+  },
+  auto_clean = false, -- não remove pacotes não utilizados
 })
 
 -- PLUGINS
 packer.startup(function(use)
-	-- o próprio packer
-	use("wbthomason/packer.nvim")
+  -- o próprio packer
+  use("wbthomason/packer.nvim")
 
-	-- requerido por muitos plugins
-	use("nvim-lua/plenary.nvim")
+  -- requerido por muitos plugins
+  use("nvim-lua/plenary.nvim")
 
-	-- popup API
-	use("nvim-lua/popup.nvim")
+  -- gerenciador de arquivos
+  use({
+    "nvim-tree/nvim-tree.lua",
+    requires = {
+      "nvim-tree/nvim-web-devicons",
+    },
+  })
 
-	-- colorschemes
-	use("marko-cerovac/material.nvim")
-	use("EdenEast/nightfox.nvim")
-	use("lunarvim/colorschemes")
+  -- popup API
+  use("nvim-lua/popup.nvim")
 
-	-- fuzzy
-	use({ "tzachar/fuzzy.nvim", require = { "nvim-telescope/telescope-fzf-native.nvim" } })
+  -- colorschemes
+  use("marko-cerovac/material.nvim")
+  use("EdenEast/nightfox.nvim")
+  use("lunarvim/colorschemes")
 
-	-- cmp plugins
-	use("hrsh7th/nvim-cmp")
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-cmdline")
-	use("hrsh7th/cmp-emoji")
-	use("hrsh7th/cmp-nvim-lua")
-	use("lukas-reineke/cmp-rg")
-	use("David-Kunz/cmp-npm")
-	use("petertriho/cmp-git")
-	use("f3fora/cmp-spell")
-	use("tzachar/cmp-fuzzy-buffer")
-	use("tzachar/cmp-fuzzy-path")
-	use("chrisgrieser/cmp-nerdfont")
-	use("pontusk/cmp-vimwiki-tags")
+  -- fuzzy
+  use({ "tzachar/fuzzy.nvim", require = { "nvim-telescope/telescope-fzf-native.nvim" } })
 
-	-- LSP
-	use("neovim/nvim-lspconfig")
-	use("williamboman/mason.nvim")
-	use("williamboman/mason-lspconfig.nvim")
-	use("jose-elias-alvarez/null-ls.nvim")
+  -- cmp plugins
+  use("hrsh7th/nvim-cmp")
+  use("hrsh7th/cmp-nvim-lsp")
+  use("hrsh7th/cmp-buffer")
+  use("hrsh7th/cmp-path")
+  use("hrsh7th/cmp-cmdline")
+  use("hrsh7th/cmp-emoji")
+  use("hrsh7th/cmp-nvim-lua")
+  use("lukas-reineke/cmp-rg")
+  use("David-Kunz/cmp-npm")
+  use("petertriho/cmp-git")
+  use("f3fora/cmp-spell")
+  use("tzachar/cmp-fuzzy-buffer")
+  use("tzachar/cmp-fuzzy-path")
+  use("chrisgrieser/cmp-nerdfont")
+  use("pontusk/cmp-vimwiki-tags")
 
-	-- Telescope
-	use({
-		"nvim-telescope/telescope.nvim",
-		requires = {
-			{ "nvim-lua/plenary.nvim" },
-			{ "nvim-telescope/telescope-github.nvim" },
-			{ "nvim-telescope/telescope-project.nvim" },
-		},
-	})
-	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
-	use("nvim-telescope/telescope-symbols.nvim")
-	use("LinArcx/telescope-env.nvim")
-	use("keyvchan/telescope-find-pickers.nvim")
-	use("adoyle-h/lsp-toggle.nvim")
-	use({
-		"benfowler/telescope-luasnip.nvim",
-		module = "telescope.extensions.luasnip",
-	})
-	use("octarect/telescope-menu.nvim")
+  -- LSP
+  use("neovim/nvim-lspconfig")
+  use("williamboman/mason.nvim")
+  use("williamboman/mason-lspconfig.nvim")
+  use("jose-elias-alvarez/null-ls.nvim")
 
-	-- Treesitter
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = function()
-			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-			ts_update()
-		end,
-	})
-	-- use 'p00f/nvim-ts-rainbow'
-	use("RRethy/nvim-treesitter-endwise")
-	use({
-		"AckslD/nvim-neoclip.lua",
-		requires = {
-			{ "kkharji/sqlite.lua", module = "sqlite" },
-		},
-		config = function()
-			require("neoclip").setup()
-		end,
-	})
+  -- Telescope
+  use({
+    "nvim-telescope/telescope.nvim",
+    requires = {
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope-github.nvim" },
+      { "nvim-telescope/telescope-project.nvim" },
+    },
+  })
+  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+  use("nvim-telescope/telescope-symbols.nvim")
+  use("LinArcx/telescope-env.nvim")
+  use("keyvchan/telescope-find-pickers.nvim")
+  use("adoyle-h/lsp-toggle.nvim")
+  use({
+    "benfowler/telescope-luasnip.nvim",
+    module = "telescope.extensions.luasnip",
+  })
+  use("octarect/telescope-menu.nvim")
 
-	-- Dashboard incial
-	use({
-		"startup-nvim/startup.nvim",
-		requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-		config = function()
-			require("startup").setup("startup.lua")
-		end,
-	})
+  -- Treesitter
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    run = function()
+      local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+      ts_update()
+    end,
+  })
+  -- use 'p00f/nvim-ts-rainbow'
+  use("RRethy/nvim-treesitter-endwise")
+  use({
+    "AckslD/nvim-neoclip.lua",
+    requires = {
+      { "kkharji/sqlite.lua", module = "sqlite" },
+    },
+    config = function()
+      require("neoclip").setup()
+    end,
+  })
 
-	-- Projects
-	use({
-		"ahmedkhalf/project.nvim",
-		config = function()
-			require("project_nvim").setup({
-				scope_chdir = "tab",
-				patterns = { ".git", "__init__.py", "package.json", ".mind" },
-			})
-		end,
-	})
+  -- Dashboard incial
+  use({
+    "startup-nvim/startup.nvim",
+    requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    config = function()
+      require("startup").setup("startup.lua")
+    end,
+  })
 
-	-- Notificações
-	use("rcarriga/nvim-notify")
+  -- Projects
+  use({
+    "ahmedkhalf/project.nvim",
+    config = function()
+      require("project_nvim").setup({
+        scope_chdir = "tab",
+        patterns = { ".git", "__init__.py", "package.json", ".mind" },
+      })
+    end,
+  })
 
-	-- GIT
-	use({
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup()
-		end,
-	})
-	use("kdheepak/lazygit.nvim")
-	use({ "sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim" })
+  -- Notificações
+  use("rcarriga/nvim-notify")
 
-	-- Modo Zen
-	use({
-		"folke/zen-mode.nvim",
-		config = function()
-			require("zen")
-		end,
-	})
+  -- GIT
+  use({
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+    end,
+  })
+  use("kdheepak/lazygit.nvim")
+  use({ "sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim" })
 
-	-- NEORG
-	use({
-		"nvim-neorg/neorg",
-		config = function()
-			require("neorg").setup({
-				load = {
-					["core.defaults"] = {},
-					["core.integrations.telescope"] = {},
-					["core.norg.concealer"] = {},
-					["core.export.markdown"] = {},
-					["core.presenter"] = {
-						config = {
-							zen_mode = "zen-mode",
-						},
-					},
-					["core.export"] = {
-						config = {
-							extensions = "all",
-						},
-					},
-					["core.gtd.base"] = {
-						config = {
-							workspace = "tasks",
-						},
-					},
-					["core.norg.dirman"] = {
-						config = {
-							workspaces = {
-								norgs = "~/Documentos/Norgs",
-								tasks = "~/Documentos/Norgs/Tasks",
-							},
-							autochdir = true,
-							index = "index.norg",
-							defaut_workspace = "~/Documentos/Norgs",
-						},
-					},
-					["core.norg.completion"] = {
-						config = {
-							engine = "nvim-cmp",
-						},
-					},
-					["core.keybinds"] = {
-						config = {
-							default_keybindins = true,
-							neorg_leader = "<leader>o",
-						},
-					},
-				},
-			})
-		end,
-		-- ft = 'norg',
-		after = "nvim-treesitter",
-		run = ":Neorg sync-parsers",
-		requires = "nvim-neorg/neorg-telescope",
-	})
+  -- Modo Zen
+  use({
+    "folke/zen-mode.nvim",
+    config = function()
+      require("zen")
+    end,
+  })
 
-	-- Whichkey
-	use({
-		"folke/which-key.nvim",
-		config = function()
-			require("whichkey")
-			require("which-key").setup({
-				window = {
-					border = "shadow",
-					winblend = 1,
-				},
-				layout = {
-					heigh = { min = 6, max = 12 },
-					width = { min = 20, max = 100 },
-					align = "center",
-				},
-			})
-		end,
-	})
+  -- NEORG
+  use({
+    "nvim-neorg/neorg",
+    config = function()
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {},
+          ["core.integrations.telescope"] = {},
+          ["core.norg.concealer"] = {},
+          ["core.export.markdown"] = {},
+          ["core.presenter"] = {
+            config = {
+              zen_mode = "zen-mode",
+            },
+          },
+          ["core.export"] = {
+            config = {
+              extensions = "all",
+            },
+          },
+          ["core.gtd.base"] = {
+            config = {
+              workspace = "tasks",
+            },
+          },
+          ["core.norg.dirman"] = {
+            config = {
+              workspaces = {
+                norgs = "~/Documentos/Norgs",
+                tasks = "~/Documentos/Norgs/Tasks",
+              },
+              autochdir = true,
+              index = "index.norg",
+              defaut_workspace = "~/Documentos/Norgs",
+            },
+          },
+          ["core.norg.completion"] = {
+            config = {
+              engine = "nvim-cmp",
+            },
+          },
+          ["core.keybinds"] = {
+            config = {
+              default_keybindins = true,
+              neorg_leader = "<leader>o",
+            },
+          },
+        },
+      })
+    end,
+    -- ft = 'norg',
+    after = "nvim-treesitter",
+    run = ":Neorg sync-parsers",
+    requires = "nvim-neorg/neorg-telescope",
+  })
 
-	-- Icons
-	use("kyazdani42/nvim-web-devicons")
-	--[[ use {
+  -- Whichkey
+  use({
+    "folke/which-key.nvim",
+    config = function()
+      require("whichkey")
+      require("which-key").setup({
+        window = {
+          border = "shadow",
+          winblend = 1,
+        },
+        layout = {
+          heigh = { min = 6, max = 12 },
+          width = { min = 20, max = 100 },
+          align = "center",
+        },
+      })
+    end,
+  })
+
+  -- Icons
+  use("kyazdani42/nvim-web-devicons")
+  --[[ use {
     'yamatsum/nvim-nonicons',
     requires = 'kyazdani42/nvim-web-devicons'
   } ]]
 
-	-- Project Mindmap
-	use({
-		"phaazon/mind.nvim",
-		config = function()
-			require("mind").setup()
-		end,
-	})
+  -- Project Mindmap
+  use({
+    "phaazon/mind.nvim",
+    config = function()
+      require("mind").setup()
+    end,
+  })
 
-	-- DAP(debugger)
-	use("mfussenegger/nvim-dap")
+  -- DAP(debugger)
+  use("mfussenegger/nvim-dap")
 
-	-- snippets
-	use("L3MON4D3/LuaSnip")
-	use("saadparwaiz1/cmp_luasnip")
-	use("rafamadriz/friendly-snippets")
+  -- snippets
+  use("L3MON4D3/LuaSnip")
+  use("saadparwaiz1/cmp_luasnip")
+  use("rafamadriz/friendly-snippets")
 
-	-- Autopairs
-	use("windwp/nvim-autopairs")
-	use("windwp/nvim-ts-autotag")
+  -- Autopairs
+  use("windwp/nvim-autopairs")
+  use("windwp/nvim-ts-autotag")
 
-	-- Comments
-	use({
-		"numToStr/Comment.nvim",
-		config = function()
-			require("Comment").setup()
-		end,
-	})
-	use("JoosepAlviste/nvim-ts-context-commentstring")
+  -- Comments
+  use({
+    "numToStr/Comment.nvim",
+    config = function()
+      require("Comment").setup()
+    end,
+  })
+  use("JoosepAlviste/nvim-ts-context-commentstring")
 
-	-- Buffers em tabs
-	use({
-		"akinsho/bufferline.nvim",
-		tag = "v3.*",
-		requires = "nvim-tree/nvim-web-devicons",
-	})
-	--use 'moll/vim-bbye'
+  -- Buffers em tabs
+  use({
+    "akinsho/bufferline.nvim",
+    tag = "v3.*",
+    requires = "nvim-tree/nvim-web-devicons",
+  })
+  --use 'moll/vim-bbye'
 
-	-- Ranger
-	use("kevinhwang91/rnvimr")
+  -- Ranger
+  use("kevinhwang91/rnvimr")
 
-	-- Terminal
-	use({
-		"akinsho/toggleterm.nvim",
-		tag = "*",
-		config = function()
-			require("toggleterm").setup()
-		end,
-	})
+  -- Terminal
+  use({
+    "akinsho/toggleterm.nvim",
+    tag = "*",
+    config = function()
+      require("toggleterm").setup()
+    end,
+  })
 
-	-- WakaTime time tracker
-	use("wakatime/vim-wakatime")
+  -- WakaTime time tracker
+  use("wakatime/vim-wakatime")
 
-	-- Java
-	use({ "mfussenegger/nvim-jdtls" })
+  -- Java
+  use({ "mfussenegger/nvim-jdtls" })
 
-	-- FSRead (melhora leitura)
-	use("nullchilly/fsread.nvim")
+  -- FSRead (melhora leitura)
+  use("nullchilly/fsread.nvim")
 
-	-- ChatGPT
-	use({
-		"terror/chatgpt.nvim",
-		run = "pip3 install -r requirements.txt",
-	})
+  -- ChatGPT
+  use({
+    "terror/chatgpt.nvim",
+    run = "pip3 install -r requirements.txt",
+  })
 
-	if PACKER_BOOTSTRAP then
-		require("packer").sync()
-	end
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
 end)
